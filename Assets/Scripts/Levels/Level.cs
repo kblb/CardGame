2c63a720c46ccc9c29aaa1467ef051e5564f7d45
luke.biel <lukasz.p.biel@gmail.com>
2023-01-06 10:47:10 +0100

    Update Unity to 2022.2.1f1

---
+++ Assets/Scripts/Levels/Level.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Enemies;
+
+namespace Levels
+{
+    [Serializable]
+    public class Level
+    {
+        public List<EnemyModel> listOfEnemies = new List<EnemyModel>();
+    }
+}
---
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using Managers;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Levels
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Level currentLevel;
+        [SerializeField, SceneObjectsOnly]
+        private EnemyManager enemyManager;
+    }
+}
