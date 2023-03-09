771109506b5bd85421bccd2a827c7cfa4ddf6551
Krzysztof Bobnis <kbobnis@gmail.com>
2023-03-09 11:04:00 +0100

    Added attack multiply effect.

---
+++ Assets/Scripts/Interface Implementations/Cards/IModify/AddNeighboursAttackModify.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class AddNeighboursAttackModify : IModify
+{
+    public AddNeighboursAttackModify()
+    {
+    }
+    
+    public void Modify(CastInstance castInstance)
+    {
+        throw new Exception("Not yet supported.");
+    }
+}
---
+++ Assets/Scripts/Interface Implementations/Cards/IModify/MultiplyAttackModify.cs
@@ -0,0 +1,14 @@
+﻿public class MultiplyAttackModify : IModify
+{
+    public readonly int damageToMultiply;
+
+    public MultiplyAttackModify(int damageToMultiply)
+    {
+        this.damageToMultiply = damageToMultiply;
+    }
+    
+    public void Modify(CastInstance castInstance)
+    {
+        castInstance.damage *= damageToMultiply;
+    }
+}
